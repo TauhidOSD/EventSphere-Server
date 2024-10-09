a94a0987a26080ce447198673d76bb72d9305fc0
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-10-09 07:11:25 +0600

    filtering for update code

--- src/controller/event/event.controller.js
+++ src/controller/event/event.controller.js
@@ -1,53 +1,126 @@
 const Event = require("../../models/Event");
 const { ObjectId } = require('mongodb');
+const { startOfToday, addDays, startOfWeek, addMonths } = require('date-fns');
 
 const getAllEvent = async (req, res) => {
-    try {
-      const allEvent = await Event.find();
-      res.status(200).json(allEvent);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
-  
-  
- 
- 
-  const getSingleEvent = async (req, res) => {
-      try {
-          const query = { _id: new ObjectId(req.params.id) }; 
-          // console.log(query);
-          const result = await Event.findOne(query);
-          
-          if (!result) {
-              return res.status(404).send({ message: "Event Not Found" });
-          }
-  
-          res.send(result);
-  
-      } catch (err) {
-          console.log(err);
-          res.status(500).send({ message: "Server Error" });
-      }
+  const {
+    category,
+    minimumPrice,
+    maximumPrice,
+    type,
+    country,
+    city,
+    startDate,
+    endDate,
+    limit = 6, 
+    page = 1, 
+  } = req.query;
+
+  const filters = {};
+
+  // Category filter
+  if (category) {
+    filters.category = category;
   }
-  
 
-  // create user
-  const createEvent = async (req, res) => {
+  // Type filter
+  if (type) {
+    filters.type = type;
+  }
 
-    const event = req.body;
-    try {
-    
-      await Event.create(event)
-      res.send({
-        success: true,
-        message: "Created successfull",
-      })
-    } catch (error) {
-      res.send({
-        success: false,
-        message: error.message,
-      })
+  // Location filters
+  if (country) {
+    filters['location.country'] = country;
+  }
+  if (city) {
+    filters['location.city'] = city;
+  }
+
+  // Date filters
+  if (startDate || endDate) {
+    filters.dateTime = {};
+
+    if (startDate) {
+      filters.dateTime.$gte = new Date(startDate);
     }
-  };
-  module.exports = { getAllEvent,createEvent,getSingleEvent};
+
+    if (endDate) {
+      filters.dateTime.$lte = new Date(endDate);
+    }
+  }
+
+  // Price filters
+  if (minimumPrice || maximumPrice) {
+    filters.price = {};
+    if (minimumPrice) filters.price.$gte = Number(minimumPrice);
+    if (maximumPrice) filters.price.$lte = Number(maximumPrice);
+  }
+
+  const itemsPerPage = parseInt(limit); // Number of items per page
+  const currentPage = parseInt(page); // Current page number
+
+  try {
+    // Counting total events
+    const totalEvents = await Event.countDocuments(filters);
+
+    // Finding filtered events with pagination
+    const events = await Event.find(filters)
+      .skip((currentPage - 1) * itemsPerPage) 
+      .limit(itemsPerPage); 
+
+    res.json({
+      totalEvents, 
+      currentPage, 
+      totalPages: Math.ceil(totalEvents / itemsPerPage), 
+      events, 
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+
+
+
+
+
+
+
+const getSingleEvent = async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    // console.log(query);
+    const result = await Event.findOne(query);
+
+    if (!result) {
+      return res.status(404).send({ message: "Event Not Found" });
+    }
+
+    res.send(result);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "Server Error" });
+  }
+}
+
+
+// create user
+const createEvent = async (req, res) => {
+
+  const event = req.body;
+  try {
+
+    await Event.create(event)
+    res.send({
+      success: true,
+      message: "Created successfully",
+    })
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    })
+  }
+};
+module.exports = { getAllEvent, createEvent, getSingleEvent };
