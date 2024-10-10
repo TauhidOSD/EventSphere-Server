9070e9c84c964a86103d47bc56fc78018a78a572
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-10-10 16:09:15 +0600

    set our main website link in cors

--- api/app.js
+++ api/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:3000", "http://localhost:3001",
+      "http://localhost:3000", "http://localhost:3001", "https://event-sphere-bice.vercel.app",
     ],
     credentials: true,
   })
