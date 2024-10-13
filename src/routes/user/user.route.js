5a65828602885ebe8c94e478bfb02ed2d26c9474
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-10-14 00:59:34 +0600

    follow system added

--- src/routes/user/user.route.js
+++ src/routes/user/user.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getSingleUser, createUser, updateUser } = require("../../controller/user/user.controller");
+const { getSingleUser, createUser, updateUser, addedFollower } = require("../../controller/user/user.controller");
 
 
 router.get("/user/:email", getSingleUser);
 router.post("/user", createUser);
 router.put("/user/:email", updateUser);
+router.put("/userAddedFollower", addedFollower);
 
 
 module.exports = router;
