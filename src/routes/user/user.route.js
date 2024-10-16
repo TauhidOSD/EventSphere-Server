1e1745abbea0207a21bc794f69a3c18dd8e4b2d9
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-10-16 20:14:14 +0600

    Get All User Filtering By User roll: user && roll: organizer

--- src/routes/user/user.route.js
+++ src/routes/user/user.route.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getSingleUser, createUser, updateUser, beOrganizer, getOrganizerRequest, getUserRollUpdatedId, userRollUpdate, organizerRequestCancel, getAllUser, blockUser } = require("../../controller/user/user.controller");
+const { getSingleUser, createUser, updateUser, beOrganizer, getOrganizerRequest, getUserRollUpdatedId, userRollUpdate, organizerRequestCancel, getAllUser } = require("../../controller/user/user.controller");
 
 
 router.get("/user", getAllUser);
 router.get("/user/:email", getSingleUser);
 router.get("/userRollUpdated/:id", getUserRollUpdatedId);
 router.put("/userRollUpdated/:id", userRollUpdate);
-router.put("/blockedUser/:id", blockUser);
 router.put("/organizingRequestCancel/:id", organizerRequestCancel);
 router.post("/user", createUser);
 router.put("/user/:email", updateUser);
